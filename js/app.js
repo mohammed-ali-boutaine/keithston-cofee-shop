c36a401c103691e934b1487671630faf9f001ab3
ali <aliboutaine987@gmail.com>
2024-10-18 12:48:13 +0100

    new version : finish pc design and i add some media queries and finish page sript

--- js/app.js
+++ js/app.js
@@ -1,19 +1,28 @@
-
-
-
-const nav =document.querySelector("nav");
+const nav = document.querySelector("nav");
 const toTopButton = document.querySelector(".toTop");
+const menu = document.querySelector(".menu");
+const links = document.querySelector(".links");
 
-toTopButton.addEventListener("click",() => {
-  window.scrollTo({ top: 0, behavior: 'smooth' });})
+toTopButton.addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
+
+window.onscroll = function () {};
 
 window.onscroll = function () {
-    if (window.scrollY > 0) {
-      nav.classList.add("shrink");
-    } else {
-      nav.classList.remove("shrink");
-    }
-  };
-  
+  if (window.scrollY > 0) {
+    nav.classList.add("shrink");
+  } else {
+    nav.classList.remove("shrink");
+  }
 
-  
+  if (window.scrollY > 700) {
+    toTopButton.classList.remove("active");
+  } else {
+    toTopButton.classList.add("active");
+  }
+};
+
+menu.addEventListener("click", () => {
+  links.classList.toggle("active");
+});
